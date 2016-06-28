5610d0f73b1975b8209eaabe970f635e34f723c2
Talita Canadinhas <talitacanadinhas@gmail.com>
2016-06-28 10:05:25 -0300

    Upload project
    What i have done until now :smile:
    
    * scenes manager by time
    * "menu" scene

---
+++ Assets/scripts/changeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+	
+	// Use this for take a random scene
+	public void ChangeScene(){
+		
+		Application.LoadLevel(Random.Range(1, Application.levelCount));
+	}
+}
---
+++ Assets/scripts/scenesManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scenesManager : MonoBehaviour {
+
+	public float deadline;	// tempo para o minigame acabar
+	public int currentLevel, nextLevel; // cena atual e a proxima cena
+
+	// Use this for initialization
+	void Start () {
+
+		deadline = 2;
+		currentLevel = Application.loadedLevel;
+	}
+
+	// Use this for take a random scene
+	public void changeScene(){
+
+		nextLevel = Random.Range(1, Application.levelCount);
+		if(currentLevel == nextLevel)
+			nextLevel = Random.Range(1, Application.levelCount);
+		Application.LoadLevel(nextLevel); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		 
+		deadline -= Time.deltaTime;
+		Debug.Log (deadline);
+		if (deadline <= 0)
+				changeScene ();
+	}
+}
